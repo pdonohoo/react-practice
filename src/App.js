86811518c6959afb088d09e4fb569196114fa53c
Preston Donohoo <preston.s.donohoo@gmail.com>
2019-02-15 21:43:07 -0700

    finished with both

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import './App.css';
 class App extends Component {
  
 state = {
-  eventChange: ''
+  input: '',
+  moviesToWatch: ['Lord of the rings', 'Baywatch', 'Harry potter', 'The Accountant', 'Black Hawk Down', 'Mulan', 'Toy Story', 'Sixteen Candles'],
+  
 }
 
-printScreen = (a) => this.setState({
-  eventChange: a.target.value
-})
-  
+printScreen = (event) => this.setState({
+  input: event.target.value
+});
+
+randomMovie = () => {
+  const randomIndex = Math.floor(Math.random()*this.state.moviesToWatch.length)
+  const movie = this.state.moviesToWatch[randomIndex]
+  alert(movie)
+};
 
   render() {
     return (
       <div>
         <div>text box: <input onChange={this.printScreen} type="text" /></div>
-        <div>Text display:{this.state.eventChange} </div>
+        <div>Text display:{this.state.input} </div>
+        <div>Movie Generator: <button type="button" onClick={this.randomMovie}> generate random movie</button></div>
       </div>
     );
   }
