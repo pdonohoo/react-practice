dc18e5d8f780a043ba1e66fe2673cc51c2323f80
Preston Donohoo <preston.s.donohoo@gmail.com>
2019-02-25 17:59:51 -0700

    updated with adding to array

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ class App extends Component {
  
 state = {
   input: '',
-  moviesToWatch: ['Lord of the rings', 'Baywatch', 'Harry potter', 'The Accountant', 'Black Hawk Down', 'Mulan', 'Toy Story', 'Sixteen Candles'],
-  
+  moviesToWatch: ['Lord of the rings, ', ' Baywatch, ', ' Harry potter, ', ' The Accountant, ', ' Black Hawk Down, ', 'Mulan, ', 'Toy Story, ', 'Sixteen Candles,'],
 }
 
 printScreen = (event) => this.setState({
@@ -19,12 +18,35 @@ randomMovie = () => {
   alert(movie)
 };
 
+addMovieToList = () => this.setState ({
+  moviesToWatch: this.state.moviesToWatch.concat(' ' + [this.state.input] + ', ')
+});
+
+
+
   render() {
+    const items = this.state.moviesToWatch.map((movie) =>
+    <li>{movie}<button onClick={this.clearMovie} id='clear'>x</button></li>
+    )
     return (
       <div>
-        <div>text box: <input onChange={this.printScreen} type="text" /></div>
-        <div>Text display:{this.state.input} </div>
-        <div>Movie Generator: <button type="button" onClick={this.randomMovie}> generate random movie</button></div>
+        <div id='top'> 
+          <div id='title'>Key-Logger</div><br></br>
+          <div>Text Box: <input onChange={this.printScreen} type="text" /> 
+          <button onClick={this.addMovieToList}>add to the movie list</button>
+          <div id='items'>Text display:{this.state.input} </div>
+          </div>
+          <ul> 
+          {items}
+        </ul>
+        </div>
+
+        <br></br>
+        
+        <div id='items'>Movie Generator: <button type="button" onClick={this.randomMovie}> generate random movie</button></div>
+
+        
+        
       </div>
     );
   }
